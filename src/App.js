4c5aaf461bf0ecec5889062a3987e5b01b1ee1b1
FernandoOnate <ofertar.fernando@gmail.com>
2022-07-13 15:08:07 -0500

    Creo el proyecto y agrego nuevo componente de prueba

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+import HolaMundo from './components/HolaMundo'
+export function Saludo() {
+  return (
+    <p>Hola, este es un saludo desde un nuevo componente en el componente App</p>
+  );
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Primeras interacciones con react, esto es un h1</h1>
+        <p>Vamos a aprender react desde 0. Hola mundo.</p>
+        <HolaMundo />
+        <Saludo />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprender react.
         </a>
       </header>
     </div>
---
+++ src/components/HolaMundo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function HolaMundo() {
+    return (
+        <div>
+            <p>Un párrafo diciendo hola mundo desde un nuevo componente.</p>
+        </div>
+    );
+}
+//export default HolaMundo;
