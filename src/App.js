baac756c343fb8a2fe0d1858ccb9a563f868ef40
FernandoOnate <ofertar.fernando@gmail.com>
2022-07-26 17:21:34 -0500

    quito los botones y las rutas, solo dejo la raiz

--- src/App.js
+++ src/App.js
@@ -7,24 +7,25 @@ import WizzardForm from './components/WizzardForm'
 function App() {
   return (
     <div className="container-fluid">
-      <Router basename="/react-app">
-        <Stack direction="horizontal" gap={2}>
-          <Link to="/">
-            {/* <Button>Ir a Home</Button> */}
-          </Link>
-          <Link to="/test-wizzard-form">
-            {/* <Button>Formuladio wizzard de prueba</Button> */}
-          </Link>
-          <Link to="/wizzard2">
-            <Button>Formuladio wizzard2</Button>
-          </Link>
-        </Stack>
-        <Routes>
-          <Route path="/test-wizzard-form" element={<FormWizzard />} />
-          <Route path="/wizzard2" element={<WizzardForm />} />
-          <Route path="/" />
-        </Routes>
-      </Router>
+      {/* <Router basename="/react-app"> */}
+      {/* <Stack direction="horizontal" gap={2}> */}
+      {/* <Link to="/"> */}
+      {/* <Button>Ir a Home</Button> */}
+      {/* </Link> */}
+      {/* <Link to="/test-wizzard-form"> */}
+      {/* <Button>Formuladio wizzard de prueba</Button> */}
+      {/* </Link> */}
+      {/* <Link to="/wizzard2"> */}
+      {/* <Button>Formuladio wizzard2</Button> */}
+      {/* </Link> */}
+      {/* </Stack> */}
+      {/* <Routes> */}
+      {/* <Route path="/test-wizzard-form" element={<FormWizzard />} /> */}
+      {/* <Route path="/wizzard2" element={<WizzardForm />} /> */}
+      {/* <Route path="/" /> */}
+      {/* </Routes> */}
+      {/* </Router> */}
+      <WizzardForm />
     </div>
   );
 }
