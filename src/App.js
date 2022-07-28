e29afc4e2cc1f33bb4d4351070a409149503eabf
FernandoOnate <ofertar.fernando@gmail.com>
2022-07-27 23:53:49 -0500

    elimino los archivos del formulario antiguo y limpio la app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Button, Stack } from "react-bootstrap";
-import FormWizzard from "./components/FormWizzard";
-import WizzardForm from './components/WizzardForm'
+import WizzardForm from "./components/WizzardForm";
 
 function App() {
   return (
     <div className="container-fluid">
-      {/* <Router basename="/react-app"> */}
-      {/* <Stack direction="horizontal" gap={2}> */}
-      {/* <Link to="/"> */}
-      {/* <Button>Ir a Home</Button> */}
-      {/* </Link> */}
-      {/* <Link to="/test-wizzard-form"> */}
-      {/* <Button>Formuladio wizzard de prueba</Button> */}
-      {/* </Link> */}
-      {/* <Link to="/wizzard2"> */}
-      {/* <Button>Formuladio wizzard2</Button> */}
-      {/* </Link> */}
-      {/* </Stack> */}
-      {/* <Routes> */}
-      {/* <Route path="/test-wizzard-form" element={<FormWizzard />} /> */}
-      {/* <Route path="/wizzard2" element={<WizzardForm />} /> */}
-      {/* <Route path="/" /> */}
-      {/* </Routes> */}
-      {/* </Router> */}
       <WizzardForm />
     </div>
   );
